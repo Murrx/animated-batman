0c721e038e3d21f94b9be87e54882fda5dd9c9ff
Robin Altena <altenarobin@gmail.com>
2014-06-26 20:28:11 +0200

    ibanrequest.xsd fixed

---
+++ ibanInterface/src/main/java/com/gsos/dimiter_robin/App.java
@@ -0,0 +1,13 @@
+package com.gsos.dimiter_robin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
