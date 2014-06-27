90da24a92b80829483e788c1b7edeed4c02124b5
Robin Altena <altenarobin@gmail.com>
2014-06-27 17:18:40 +0200

    hopefully ignore target files from now on

--- ibanInterface/target/generated-sources/wsimport/com/gsos/dimiter_robin/ibanInterface/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://gsos.com/dimiter_robin/ibanrequest", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.gsos.dimiter_robin.ibanInterface;
