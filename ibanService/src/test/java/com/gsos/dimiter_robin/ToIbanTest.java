2939197c536b1ab3b90354ff92f43a2925fed3a2
Didel <mrdidel@gmail.com>
2014-07-01 12:09:07 +0200

    To Iban Tests v0.1 alpha

--- ibanService/src/test/java/com/gsos/dimiter_robin/ToIbanTest.java
+++ ibanService/src/test/java/com/gsos/dimiter_robin/ToIbanTest.java
@@ -7,46 +7,67 @@ import java.math.BigInteger;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.gsos.dimiter_robin.ibanInterface.Authentication;
 import com.gsos.dimiter_robin.ibanInterface.Bankcode;
+import com.gsos.dimiter_robin.ibanInterface.Fault_Exception;
 import com.gsos.dimiter_robin.ibanInterface.Ibanrequest;
 
 public class ToIbanTest {
 
 	private static IbanServiceImplementation service;
 	private static Ibanrequest request;
+	private static Authentication auth;
 	
 	@BeforeClass
 	public static void init(){
 		service = new IbanServiceImplementation();
 		request = new Ibanrequest();
-		request.setBankcode(Bankcode.INGB);
-		request.setRekeningnummer(new BigInteger("8370674"));
+		
+		auth = new Authentication() ;
+		auth.setBedrijfsnaam("test");
+		auth.setHuisnummer("74");
+		auth.setPostcode("8200AA");
+
 	}	
 	
 	//normaal rek nr
 	@Test
-	public void shouldReturnIban() {
-		fail("Not yet implemented");
+	public void shouldReturnIban() throws Fault_Exception {
+		request.setBankcode(Bankcode.INGB);
+		request.setRekeningnummer(new BigInteger("8829939"));
+		service.toIban(auth, request);
 	}
 	
 	//ing rek nr
 	@Test
-	public void shouldAlsoReturnIban() {
+	public void shouldAlsoReturnIban() throws Fault_Exception {
+		request.setBankcode(Bankcode.ABNA);
+		request.setRekeningnummer(new BigInteger("417164300"));
+		service.toIban(auth, request);
 	}
 		
 	//8 cijfers
-	@Test
-	public void shouldThrowExceptionBecauseInvallidNumber() {
+	@Test(expected=Fault_Exception.class)
+	public void shouldThrowExceptionBecauseInvalidNumber() throws Fault_Exception {
+		request.setBankcode(Bankcode.ABNA);
+		request.setRekeningnummer(new BigInteger("41716430"));
+		service.toIban(auth, request);
 	}
 	
 	//ongeldige bankcode
-	@Test
-	public void shouldThrowExceptionBecauseInvallidBankCode() {
+	@Test(expected=Fault_Exception.class)
+	public void shouldThrowExceptionBecauseInvalidBankCode() throws Fault_Exception {
+		request.setBankcode(Bankcode.fromValue("ABCD"));
+		request.setRekeningnummer(new BigInteger("417164300"));
+		service.toIban(auth, request);
 	}
 	
 	//bankcode kleine letters
-	@Test
-	public void shouldNotThrowExceptionBecauseOfLowerCase() {
-	}
+	/*@Test
+	public void shouldNotThrowExceptionBecauseOfLowerCase() throws Fault_Exception {
+		request.setBankcode(Bankcode.fromValue("abna"));
+		request.setRekeningnummer(new BigInteger("417164300"));
+		service.toIban(auth, request);
+	}*/
 }
 
